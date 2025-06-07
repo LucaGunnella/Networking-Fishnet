6cb09f80c3af16a94b030dc16bb6a1427c34eb5c
LucaGunnella <luca.gunnella@gmail.com>
2025-06-07 15:54:04 +0200

    add TextmeshPro

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@ using FishNet.Managing.Logging;
 using UnityEngine;
 using FishNet.Object;
 
+// NetworkBehaviour gives access to all network functionalities and forces gameObject to be NetworkObject.
 public class PlayerMovement : NetworkBehaviour
 {
     [SerializeField] private float _moveSpeed = 5f;
@@ -11,7 +12,9 @@ public class PlayerMovement : NetworkBehaviour
         Move();
     }
 
-    //Usiamo l'attributo Client per assicurarci che solo l'owner possa muovere il proprio Player
+    // Using Client attribute to force execution only for the owner of this NetworkObject.
+    // LoggingType = if and what message should be broadcast in console upon not meeting requirements.
+    // RequireOwnership = if ownership is required to execute this method.
     [Client(Logging = LoggingType.Off, RequireOwnership = true)]
     private void Move()
     {
