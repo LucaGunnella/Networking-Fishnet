f5f637703bbe2dc0f22bec4c79d0a8262afb34d9
Luca Gunnella <luca.gunnella@gmail.com>
2025-06-13 15:48:04 +0200

    finish camera look

--- Assets/Scripts/Exercises/LookComponent.cs
+++ Assets/Scripts/Exercises/LookComponent.cs
@@ -1,3 +1,4 @@
+using System;
 using FishNet.Connection;
 using FishNet.Managing.Logging;
 using FishNet.Object;
@@ -7,6 +8,11 @@ public class LookComponent : NetworkBehaviour
 {
     [SerializeField] private Transform _cameraSocket;
     [SerializeField] private GameObject _cameraPrefab;
+    [SerializeField] private float mouseSensitivity = 100f;
+
+    private Camera _camera;
+    private Transform _root;
+    private float _xRotation;
 
     public override void OnOwnershipClient(NetworkConnection prevOwner)
     {
@@ -14,9 +20,39 @@ public class LookComponent : NetworkBehaviour
         CreateCamera();
     }
 
+    private void Awake()
+    {
+        _root = transform.root;
+    }
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    
+    private void Update()
+    {
+        Look();
+    }
+
+    [Client(Logging = LoggingType.Off, RequireOwnership = true)]
+    private void Look()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+
+        _camera.transform.parent.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+        _root.Rotate(Vector3.up * mouseX);
+    }
+
     [Client(Logging = LoggingType.Off, RequireOwnership = true)]
     private void CreateCamera()
     {
-        Instantiate(_cameraPrefab, _cameraSocket);
+        _camera = Instantiate(_cameraPrefab, _cameraSocket).GetComponent<Camera>();
     }
+    
 }
