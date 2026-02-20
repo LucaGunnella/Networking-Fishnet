78f5bc19d83016ffcc50cf536e5dea180a799340
Fabio Pelosini <fabio.pelosini99@gmail.com>
2026-02-20 14:47:14 +0100

    refactor

--- Assets/Scripts/Exercises/Exc01/Barrel.cs
+++ Assets/Scripts/Exercises/Exc01/Barrel.cs
@@ -3,18 +3,11 @@ using FishNet.Object;
 
 public class Barrel : NetworkBehaviour
 {
-    [SerializeField] private NetworkObject _cubePrefab;
     [SerializeField] private Animator _animator;
-
-    void Awake()
-    {
-        
-    }
 
     [ServerRpc(RequireOwnership = false)]
     public void Explode()
     {
-
         if(_animator != null)
             _animator.SetTrigger("Explode");
     }
