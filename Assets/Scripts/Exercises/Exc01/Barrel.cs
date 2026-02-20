a8a7e6cba30c450d7f8e3b59fcab2441804f2dad
Fabio Pelosini <fabio.pelosini99@gmail.com>
2026-02-20 14:41:46 +0100

    fix animation

--- Assets/Scripts/Exercises/Exc01/Barrel.cs
+++ Assets/Scripts/Exercises/Exc01/Barrel.cs
@@ -6,21 +6,14 @@ public class Barrel : NetworkBehaviour
     [SerializeField] private NetworkObject _cubePrefab;
     [SerializeField] private Animator _animator;
 
-    private MeshRenderer _meshRenderer;
-
     void Awake()
     {
-        if(gameObject.TryGetComponent<MeshRenderer>(out var meshRenderer))
-        
-            _meshRenderer = meshRenderer;
         
     }
 
     [ServerRpc(RequireOwnership = false)]
     public void Explode()
     {
-        if(_meshRenderer != null)
-            _meshRenderer.enabled = false;
 
         if(_animator != null)
             _animator.SetTrigger("Explode");
