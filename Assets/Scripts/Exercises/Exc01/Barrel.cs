39002a6ffeb4210cd882394445568654e701921f
Luigi Magnanimo <diddimagnanimolm@gmail.com>
2026-02-20 15:07:36 +0100

    feat: barrel do damage

--- Assets/Scripts/Exercises/Exc01/Barrel.cs
+++ Assets/Scripts/Exercises/Exc01/Barrel.cs
@@ -4,60 +4,70 @@ using FishNet.Object;
 
 public class Barrel : NetworkBehaviour
 {
-     [SerializeField] private float _timeToDestroy = 1f;
-     [SerializeField] private Color _color = Color.red;
-     
-     private Renderer _renderer;
-     private bool _isDestroying = false;
-     
-     private Coroutine _onDestroy;
-     
-     private void Awake()
-     {
-          _renderer = GetComponent<Renderer>();
-     }
-     
-     public void OnHit()
-     {
-          if (_isDestroying) return;
-          
-          Debug.Log("Sono stato colpito");
-          
-          if (IsServerStarted)
-          {
-               _isDestroying = true;
-               RpcChangeColor(_color);
-               StartCoroutine(OnDestroyingRoutine());
-          }
-          else
-          {
-               ServerOnHit();
-          }
-     }
-     
-     [ServerRpc(RequireOwnership = false)]
-     private void ServerOnHit()
-     {
-          _isDestroying = true;
-          RpcChangeColor(_color);
-          StartCoroutine(OnDestroyingRoutine());
-     }
+    [SerializeField] private float _timeToDestroy = 1f;
+    [SerializeField] private Color _color = Color.red;
+    [SerializeField] private float _explosionRadius = 3f;
+    [SerializeField] private int _damage = 5;
 
-     private IEnumerator OnDestroyingRoutine()
-     {
-          yield return new WaitForSeconds(_timeToDestroy);
+    private Renderer _renderer;
+    private bool _isDestroying = false;
 
-          ServerManager.Despawn(gameObject);
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
 
-          yield return null;
-     }
+    public void OnHit()
+    {
+        if (_isDestroying) return;
+        
+        if (IsServerStarted) ServerProcessHit();
+        else ServerOnHit();
+    }
 
-     [ObserversRpc]
-     private void RpcChangeColor(Color newColor)
-     {
-          if (_renderer != null)
-          {
-               _renderer.material.color = newColor;
-          }
-     }
+    [ServerRpc(RequireOwnership = false)]
+    private void ServerOnHit() => ServerProcessHit();
+
+    private void ServerProcessHit()
+    {
+        if (_isDestroying) return;
+        _isDestroying = true;
+
+        RpcChangeColor(_color);
+        StartCoroutine(OnDestroyingRoutine());
+    }
+
+    private IEnumerator OnDestroyingRoutine()
+    {
+        yield return new WaitForSeconds(_timeToDestroy);
+        
+        Explode();
+
+        ServerManager.Despawn(gameObject);
+    }
+
+    private void Explode()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, _explosionRadius);
+        
+        foreach (var hit in hitColliders)
+        {
+            if (hit.TryGetComponent(out HealthComponent health))
+            {
+                health.TakeDamage(_damage);
+            }
+        }
+    }
+
+    [ObserversRpc]
+    private void RpcChangeColor(Color newColor)
+    {
+        if (_renderer != null) _renderer.material.color = newColor;
+    }
+    
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, _explosionRadius);
+    }
 }
