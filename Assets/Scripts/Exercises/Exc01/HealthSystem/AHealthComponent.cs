ffc692840a16f49b26c4eb75af3b43d8ad6997ed
Eugenio Milazzo <ioiugi04@gmail.com>
2026-02-20 13:05:39 +0100

    Chore: Created AHealthComponent class

---
+++ Assets/Scripts/Exercises/Exc01/HealthSystem/AHealthComponent.cs
@@ -0,0 +1,21 @@
+using FishNet.Object;
+using FishNet.Object.Synchronizing;
+using UnityEngine;
+
+public class AHealthComponent : NetworkBehaviour
+{
+    [SerializeField] private int _maxHealth = 100;
+    
+    public readonly SyncVar<int> Health = new();
+    
+    [ServerRpc(RequireOwnership = false)]
+    public void TakeDamage(int amount)
+    {
+        Health.Value -= amount;
+    }
+    
+    private void OnHealthChanged(int prev, int next, bool asServer)
+    {
+        Health.Value = next;
+    }
+}
