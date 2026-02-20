4ed5476946f529cfc6b2e56fb76b27e23eb9d15b
Luigi Magnanimo <diddimagnanimolm@gmail.com>
2026-02-20 14:52:53 +0100

    feat: destroying routine

--- Assets/Scripts/Exercises/Exc01/Barrel.cs
+++ Assets/Scripts/Exercises/Exc01/Barrel.cs
@@ -1,11 +1,63 @@
+using System.Collections;
 using UnityEngine;
 using FishNet.Object;
 
 public class Barrel : NetworkBehaviour
 {
+     [SerializeField] private float _timeToDestroy = 1f;
+     [SerializeField] private Color _color = Color.red;
+     
+     private Renderer _renderer;
+     private bool _isDestroying = false;
+     
+     private Coroutine _onDestroy;
+     
+     private void Awake()
+     {
+          _renderer = GetComponent<Renderer>();
+     }
+     
      public void OnHit()
      {
+          if (_isDestroying) return;
+          
           Debug.Log("Sono stato colpito");
-          Destroy(gameObject);
+          
+          if (IsServerStarted)
+          {
+               _isDestroying = true;
+               RpcChangeColor(_color);
+               StartCoroutine(OnDestroyingRoutine());
+          }
+          else
+          {
+               ServerOnHit();
+          }
+     }
+     
+     [ServerRpc(RequireOwnership = false)]
+     private void ServerOnHit()
+     {
+          _isDestroying = true;
+          RpcChangeColor(_color);
+          StartCoroutine(OnDestroyingRoutine());
+     }
+
+     private IEnumerator OnDestroyingRoutine()
+     {
+          yield return new WaitForSeconds(_timeToDestroy);
+
+          ServerManager.Despawn(gameObject);
+
+          yield return null;
+     }
+
+     [ObserversRpc]
+     private void RpcChangeColor(Color newColor)
+     {
+          if (_renderer != null)
+          {
+               _renderer.material.color = newColor;
+          }
      }
 }
