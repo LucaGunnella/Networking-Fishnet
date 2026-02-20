af96bbbd9437f536176d4a168a98ecef01903eb8
Zhan Yi Jachy <ufficiohu@gmail.com>
2026-02-20 13:10:50 +0100

    feat: test explosion

--- Assets/Scripts/Exercises/Exc01/Barrel.cs
+++ Assets/Scripts/Exercises/Exc01/Barrel.cs
@@ -1,7 +1,20 @@
+using System;
 using UnityEngine;
 using FishNet.Object;
 
 public class Barrel : NetworkBehaviour
 {
-     // Add your code
+     private float _explosionRadius;
+
+     private void BarrelExplosion()
+     {
+          // check objects inside collider, if an object has health component, Debug log
+          Collider[] hitColliders = new Collider[10];
+          int colliderCount = Physics.OverlapSphereNonAlloc(transform.position, _explosionRadius, hitColliders);
+          for (int i = 0; i < colliderCount; i++)
+          {
+               Debug.Log(hitColliders[i].gameObject.name);
+          }
+          Destroy(gameObject);
+     }
 }
